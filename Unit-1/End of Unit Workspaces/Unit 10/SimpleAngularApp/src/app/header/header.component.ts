df55faa30650c5b1c40e0bacfcfbf4a494d90b6e
driftSam <pjjuliana216@gmail.com>
2022-08-20 19:58:00 -0400

    Add Unit 1 example code

---
+++ Unit-1/End of Unit Workspaces/Unit 10/SimpleAngularApp/src/app/header/header.component.ts
@@ -0,0 +1,25 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  pageRequested = 1;
+
+  @Output()
+  pageChangedEvent = new EventEmitter<number>();
+
+  ngOnInit() {
+  }
+
+  onPageChange(page) {
+    this.pageRequested = page;
+    console.log(this.pageRequested);
+    this.pageChangedEvent.emit(page);
+  }
+}
